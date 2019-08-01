7878f1e8acbb3733a5eca9bceb4c306be90d04ce
Andrew Tan <andrewvmail@gmail.com>
2019-08-01 01:55:26 -0700

    playing around

---
+++ Podfile
@@ -0,0 +1,12 @@
+project 'xmppclient.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'xmppclient' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for xmppclient
+
+end
---
+++ xmppclient/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  xmppclient
+//
+//  Created by Andrew Tan on 2019-08-01.
+//  Copyright © 2019 Andrew Tan. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    }
+
+
+
+}
