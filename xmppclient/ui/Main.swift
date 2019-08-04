282ca95a2a6113c52b05781066e0c19f53162409
Andrew Tan <andrewvmail@gmail.com>
2019-08-03 17:24:03 -0700

    WIP todos

--- xmppclient/ui/Main.swift
+++ xmppclient/ui/Main.swift
@@ -1,6 +1,10 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+
+    var hobbies: [String] = ["Computers", "Photography", "Cars", "Reading", "Learning New Things"]
+
+
     @IBOutlet var textLabel: UILabel?
 
     var count: Int? = 0
@@ -8,8 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        observation = controller().state.observe(\.count, options: [.initial]) { (model, change) in
+        observation = controller().state.counter.observe(\.count, options: [.initial]) { (model, change) in
             self.textLabel?.text = "\(model.count)"
         }
     }
@@ -17,7 +20,18 @@ class ViewController: UIViewController {
     @IBAction func tapIncrement(sender: UIButton) {
         run(sequence: increment, name: "increment")
     }
+
     @IBAction func tapDecrement(sender: UIButton) {
         run(sequence: decrement, name: "decrement")
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = hobbies[indexPath.row]
+        return cell
     }
 }
